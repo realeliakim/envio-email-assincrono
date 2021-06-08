da90b2a1c289c89d5a9aa890322468d14a5b297a
EliakimAquino <eliakim.aquino@gmail.com>
2021-06-08 01:41:47 -0300

    Atualizaçao

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -26,7 +26,7 @@
               </div>
 
               <div class="form-group row">
-                <label for="email" class="col-md-4 col-form-label text-md-right">Eemail</label>
+                <label for="email" class="col-md-4 col-form-label text-md-right">E-mail</label>
 
                 <div class="col-md-6">
                   <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email">
